a85e56fc16450e9ae5fe3003af021018113dfdf0
s3963096 <s3963096@student.rmit.edu.au>
2025-03-25 02:34:28 +1100

    Adding new endpoint for showing all campgrounds and new view

--- app.js
+++ app.js
@@ -1,14 +1,33 @@
 const express = require("express");
+const mongoose = require("mongoose");
 const path = require("path");
-app = express();
+const Campground = require("./models/campground");
 
 const port = 3000;
+
+mongoose.connect("mongodb://127.0.0.1:27017/yelp-camp");
+const db = mongoose.connection;
+db.on("error", console.error.bind(console, "connection error:"));
+db.once("open", () => {
+  console.log("Database connected");
+});
+
+app = express();
 
 app.set("view engine", "ejs");
 app.set("views", path.join(__dirname, "views"));
 
 app.get("/", (req, res) => {
   res.render("home");
+});
+
+app.get("/campgrounds", async (req, res) => {
+  const campgrounds = await Campground.find({});
+  res.render("campgrounds/index", { campgrounds });
+});
+
+app.get("/campgrounds/:id", async (req, res) => {
+  res.render("campgrounds/show");
 });
 
 app.listen(port, () => {
